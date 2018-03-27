b606d999d6d7321178becd0b13f58def76244fe8
Alexey Belousov <belousov.fg@yandex.ru>
2018-03-27 18:11:39 +0300

    second commit

--- path-info.js
+++ path-info.js
@@ -5,23 +5,29 @@ module.exports = (path, callback) => {
     if (err) callback(err, undefined);
     if (stats.isFile()) {
       fs.readFile(path, { encoding: 'utf8' }, (err, content) => {
-        if (err) callback(err, undefined);
-        callback(null, {
-          path: path,
-          type: 'file',
-          content: content,
-          childs: undefined
-        });
+        if (err) {
+          callback(err, undefined);
+        } else {
+          callback(null, {
+            path: path,
+            type: 'file',
+            content: content,
+            childs: undefined
+          });
+        }
       });
     } else {
       fs.readdir(path, (err, files) => {
-        if (err) callback(err, undefined);
-        callback(null, {
-          path: path,
-          type: 'directory',
-          content: undefined,
-          childs: files
-        });
+        if (err) {
+          callback(err, undefined);
+        } else {
+          callback(null, {
+            path: path,
+            type: 'directory',
+            content: undefined,
+            childs: files
+          });
+        }
       });
     }
   });
